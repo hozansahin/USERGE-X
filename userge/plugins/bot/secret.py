3667bbb5d3361788ba99e3f05408efba8ee7c3b9
Hozan Şahin <hozansahin@gmail.com>
2020-09-20 20:28:53 +0300

    Secret update
    secret gizli mesaj güncellendi

--- userge/plugins/bot/secret.py
+++ userge/plugins/bot/secret.py
@@ -1,7 +1,7 @@
 # Copyright (C) 2020 BY - GitHub.com/code-rgb [TG - @deleteduser420]
 # All rights reserved.
 
-from userge import userge, Config, get_collection, Message
+from userge import userge, Config, Message
 from pyrogram.types import CallbackQuery
 from pyrogram import filters
 import json
@@ -23,27 +23,16 @@ if Config.BOT_TOKEN and Config.OWNER_ID:
         if os.path.exists(SECRETS):
             view_data = json.load(open(SECRETS))
             sender = await userge.get_me()
-            msg = f"🔓 𝗠𝗲𝘀𝘀𝗮𝗴𝗲 𝗳𝗿𝗼𝗺: {sender.first_name}"
-            if sender.last_name:
-                msg += f" {sender.last_name}\n"
-            else:
-                msg += "\n"
+            msg = f"🔓 {sender.first_name} gelen mesaj: "
+            msg += f" {sender.last_name}\n" if sender.last_name else "\n"
             data = view_data[msg_id]
             receiver =  data['user_id']
             msg += data['msg']
-            u_id = c_q.from_user.id 
-            if u_id == Config.OWNER_ID or u_id == receiver:
+            u_id = c_q.from_user.id
+            if u_id in [Config.OWNER_ID, receiver]:
                 await c_q.answer(msg, show_alert=True)
             else:
-                await c_q.answer("This Message is Confidential 👽", show_alert=True)
+                await c_q.answer("Bu Mesaj Çok Gizlidir!", show_alert=True)
         else:
-            await c_q.answer("This message doesn't exist anymore", show_alert=True)
+            await c_q.answer("Bu mesaj artık mevcut değil", show_alert=True)
 
-
-@userge.on_cmd("secret", about={
-    'header': "for help do .secret"})
-async def secret_(message: Message):
-    text = "**IN INLINE BOT**\n\n"
-    text += "secret [username OR userid] \"Your Secret Message\""
-    await message.edit(text, del_in=20)
-    
